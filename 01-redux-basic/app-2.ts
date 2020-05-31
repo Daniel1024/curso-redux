8fc5ba78ef7a70d23d59fc8fc17ba202ad0c482b
Daniel Lopez <ext_delopezp@falabella.cl>
2020-05-31 13:51:58 -0400

    Store

--- 01-redux-basic/app-2.ts
+++ 01-redux-basic/app-2.ts
@@ -1,4 +1,4 @@
-import { Action } from './ngrx-fake/ngrx';
+import { contadorReducer } from './contador/contador.reducer';
 import {
   incrementadorAction,
   decrementadorAction,
@@ -7,25 +7,8 @@ import {
   resetAction
 } from './contador/contador.actions';
 
-function reducer(state = 10, action: Action) {
-  switch (action.type) {
-    case 'INCREMENTAR':
-      return state + 1;
-    case 'DECREMENTAR':
-      return state - 1;
-    case 'MULTIPLICAR':
-      return state * action.payload;
-    case 'DIVIDIR':
-      return state / action.payload;
-    case 'RESET':
-      return 0;
-    default:
-      return state;
-  }
-}
-
-console.log('Sumar:', reducer(10, incrementadorAction)); // 11
-console.log('Restar:', reducer(10, decrementadorAction)); // 9
-console.log('Multiplicar:', reducer(10, multiplicarAction)); // 20
-console.log('Dividir:', reducer(10, dividirAction)); // 5
-console.log('Reset:', reducer(10, resetAction)); // 0
+console.log('Sumar:', contadorReducer(10, incrementadorAction)); // 11
+console.log('Restar:', contadorReducer(10, decrementadorAction)); // 9
+console.log('Multiplicar:', contadorReducer(10, multiplicarAction)); // 20
+console.log('Dividir:', contadorReducer(10, dividirAction)); // 5
+console.log('Reset:', contadorReducer(10, resetAction)); // 0
