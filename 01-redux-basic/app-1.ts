ed65e2fbb41b2be90f569cf82cc8309e42897645
Daniel Lopez <ext_delopezp@falabella.cl>
2020-05-31 13:13:38 -0400

    Acciones y reducers basico

--- 01-redux-basic/app-1.ts
+++ 01-redux-basic/app-1.ts
@@ -1,1 +1,20 @@
-console.log('Hola mundo');
+// Acciones
+interface Action {
+  type: string;
+  payload?: any;
+}
+
+const incrementadorAction: Action = {
+  type: 'INCREMENTAR'
+}
+
+function reducer(state = 10, action: Action) {
+  if (action.type === 'INCREMENTAR') {
+    return state += 1;
+  }
+  return state;
+}
+
+// Usar el reducer
+
+console.log(reducer(10, incrementadorAction));
