be11d47f4acbb92d4604c8de3f4fb77cf00b40b1
Daniel Lopez <ext_delopezp@falabella.cl>
2020-06-01 00:24:27 -0400

    Cambiar el filtro - únicamente el string

--- 03-todoapp/src/app/app.module.ts
+++ 03-todoapp/src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
 import { StoreModule } from '@ngrx/store';
-import { todoReducer } from './todos/todo.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { appReducers } from './app.reducers';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     TodoModule,
-    StoreModule.forRoot({todos: todoReducer}),
+    StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
     ReactiveFormsModule
   ],
--- 03-todoapp/src/app/todos/todo-footer/todo-footer.component.ts
+++ 03-todoapp/src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducers';
+import * as actions from '../../filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-footer.component.scss']
 })
 export class TodoFooterComponent implements OnInit {
+  filtroActual: actions.filtrosValidos = 'todos';
+  filtros: actions.filtrosValidos[] = ['todos', 'completados', 'pendientes'];
 
-  constructor() { }
+  constructor(
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+    this.store.select('filtro').subscribe(filtro =>  this.filtroActual = filtro);
+  }
+
+  cambiarFiltro(filtro: actions.filtrosValidos, $event) {
+    $event.preventDefault();
+    this.store.dispatch(actions.setFiltro({filtro}));
   }
 
 }
