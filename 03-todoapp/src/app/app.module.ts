7530a057e26464b63b5c0ab62974f27b3eb37d50
Daniel Lopez <ext_delopezp@falabella.cl>
2020-05-31 22:02:24 -0400

    Instalar NgRx, modelo, acciones y reducer de Todos

--- 03-todoapp/src/app/app.module.ts
+++ 03-todoapp/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
+import { StoreModule } from '@ngrx/store';
+import { todoReducer } from './todos/todo.reducer';
 
 @NgModule({
   declarations: [
@@ -14,9 +16,11 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TodoModule
+    TodoModule,
+    StoreModule.forRoot({todos: todoReducer})
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ 03-todoapp/src/app/todos/todo.reducer.ts
@@ -0,0 +1,13 @@
+import { createReducer, on } from '@ngrx/store';
+import { crear } from './todo.actions';
+import { TodoModels } from './models/todo.models';
+
+export const estadoInicial: TodoModels[] = [];
+
+const _todoReducer = createReducer(estadoInicial,
+  on(crear, (state, {texto}) => [...state, new TodoModels(texto)])
+);
+
+export function todoReducer(state, action) {
+  return _todoReducer(state, action);
+}
