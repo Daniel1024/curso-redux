540edaee86df69c0457d4fe5fcd901a1ef6246c2
Daniel Lopez <ext_delopezp@falabella.cl>
2020-06-03 22:52:20 -0400

    Obtener información del usuario activo en Firebase

--- 04-ingresoEgresoApp/src/app/services/auth.service.ts
+++ 04-ingresoEgresoApp/src/app/services/auth.service.ts
@@ -11,6 +11,12 @@ export class AuthService {
   ) {
   }
 
+  initAuthListener() {
+    this.auth.authState.subscribe(fuser => {
+      console.log(fuser);
+    });
+  }
+
   crearUsuario(nombre: string, email: string, password: string) {
     return this.auth.createUserWithEmailAndPassword(email, password);
   }
