695025927a2953552657045d59c5b70f8bb9a821
Daniel Lopez <ext_delopezp@falabella.cl>
2020-06-03 20:34:38 -0400

    form listo

--- 04-ingresoEgresoApp/src/app/app.module.ts
+++ 04-ingresoEgresoApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -28,9 +29,11 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- 04-ingresoEgresoApp/src/app/auth/register/register.component.ts
+++ 04-ingresoEgresoApp/src/app/auth/register/register.component.ts
@@ -1,16 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registroForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  crearUsuario() {
+    console.log(
+      this.registroForm,
+      this.registroForm.valid,
+      this.registroForm.value
+    );
   }
 
 }
