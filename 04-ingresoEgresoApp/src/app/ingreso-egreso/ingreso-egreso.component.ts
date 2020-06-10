a69c55b9db4922f991294b7e7e874784c1348586
Daniel Lopez <d.lopez.1740@gmail.com>
2020-06-09 21:02:51 -0400

    Optimizaciones de loading

--- 04-ingresoEgresoApp/src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ 04-ingresoEgresoApp/src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IngresoEgresoModel } from '../models/ingreso-egreso.model';
 import { IngresoEgresoService } from '../services/ingreso-egreso.service';
 import Swal from 'sweetalert2';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.reducer';
+import { isLoading, stopLoading } from '../shared/ui.actions';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-ingreso-egreso',
@@ -10,25 +14,36 @@ import Swal from 'sweetalert2';
   styles: [
   ]
 })
-export class IngresoEgresoComponent implements OnInit {
+export class IngresoEgresoComponent implements OnInit, OnDestroy {
 
   ingresoForm: FormGroup;
   tipo: string = 'ingreso';
+  cargando: boolean = false;
+  loadingSubs: Subscription
 
   constructor(
     private fb: FormBuilder,
-    private ingresoEgresoService: IngresoEgresoService
+    private ingresoEgresoService: IngresoEgresoService,
+    private store: Store<AppState>
   ) { }
 
   ngOnInit(): void {
+    this.loadingSubs = this.store.select('ui').subscribe(ui => this.cargando = ui.isLoading);
+
     this.ingresoForm = this.fb.group({
       descripcion: ['', Validators.required],
       monto: ['', Validators.required],
     })
   }
 
+  ngOnDestroy() {
+    this.loadingSubs.unsubscribe();
+  }
+
   async guardar() {
     if (this.ingresoForm.invalid) return;
+
+    this.store.dispatch(isLoading());
 
     const { descripcion, monto } = this.ingresoForm.value;
     const ingresoEgreso = new IngresoEgresoModel(descripcion, monto, this.tipo);
@@ -36,8 +51,10 @@ export class IngresoEgresoComponent implements OnInit {
     try {
       await this.ingresoEgresoService.crearIngresoEgreso(ingresoEgreso);
       this.ingresoForm.reset();
+      this.store.dispatch(stopLoading());
       Swal.fire('Registro creado', descripcion, 'success');
     } catch (e) {
+      this.store.dispatch(stopLoading());
       Swal.fire('Error', e.message, 'error');
     }
   }
