03508374f19cc4444d56ec3ccd86875582e6319b
Daniel Lopez <d.lopez.1740@gmail.com>
2020-06-17 16:00:15 -0400

    Usuario Service

--- 05-http-effects/src/app/users/list/list.component.ts
+++ 05-http-effects/src/app/users/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-list',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getUsers()
+      .subscribe(data => console.log(data));
   }
 
 }
