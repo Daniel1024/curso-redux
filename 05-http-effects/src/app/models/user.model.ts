881be6643dd70756d7851d583693a202f60d0732
Daniel Lopez <d.lopez.1740@gmail.com>
2020-06-17 15:46:58 -0400

    Modelo de usuario

---
+++ 05-http-effects/src/app/models/user.model.ts
@@ -0,0 +1,15 @@
+export class UserModel {
+  // public id: ,
+  // public email: ,
+  // public first_name: ,
+  // public last_name: ,
+  // public avatar: ,
+  constructor(
+    public id: number,
+    public email: string,
+    public first_name: string,
+    public last_name: string,
+    public avatar: string
+  ) {
+  }
+}
