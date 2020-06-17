e1ec75135df5ce5ac20f56f4a689832cef469596
Daniel Lopez <d.lopez.1740@gmail.com>
2020-06-17 13:00:45 -0400

    LazyLoad Store

--- 04-ingresoEgresoApp/src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ 04-ingresoEgresoApp/src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AppState } from '../../app.reducer';
 import { IngresoEgresoModel } from '../../models/ingreso-egreso.model';
 import { MultiDataSet, Label } from 'ng2-charts';
 import { Subscription } from 'rxjs';
+import { AppStateWithIngreso } from '../ingreso-egreso.reducer';
 
 @Component({
   selector: 'app-estadistica',
@@ -25,7 +25,7 @@ export class EstadisticaComponent implements OnInit, OnDestroy {
   doughnutChartData: MultiDataSet = [];
 
   constructor(
-    private store: Store<AppState>
+    private store: Store<AppStateWithIngreso>
   ) { }
 
   ngOnInit(): void {
--- 04-ingresoEgresoApp/src/app/ingreso-egreso/ingreso-egreso.reducer.ts
+++ 04-ingresoEgresoApp/src/app/ingreso-egreso/ingreso-egreso.reducer.ts
@@ -1,14 +1,19 @@
-import {createReducer, on} from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import * as actions from './ingreso-egreso.actions';
 import { IngresoEgresoModel } from '../models/ingreso-egreso.model';
+import { AppState } from '../app.reducer';
 
 export interface State {
   items: IngresoEgresoModel[];
 }
 
+export interface AppStateWithIngreso extends AppState {
+  ingresosEgresos: State
+}
+
 export const initialState: State = {
   items: []
-}
+};
 
 const _ingresoEgresoReducer = createReducer(initialState,
   on(actions.setItems, (state, {items}) => ({
@@ -17,7 +22,7 @@ const _ingresoEgresoReducer = createReducer(initialState,
   })),
 
   on(actions.unsetItems, state => ({...state, items: []}))
-)
+);
 
 export function ingresoEgresoReducer(state, action) {
   return _ingresoEgresoReducer(state, action);
