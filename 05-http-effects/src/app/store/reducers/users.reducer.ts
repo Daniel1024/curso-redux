2742c710a93bc376bf150cd278fd6148fd441070
Daniel Lopez <d.lopez.1740@gmail.com>
2020-06-17 17:55:09 -0400

    Reducer de Usuarios

---
+++ 05-http-effects/src/app/store/reducers/users.reducer.ts
@@ -0,0 +1,39 @@
+import { createReducer, on } from '@ngrx/store';
+import { loadUsers, loadUsersFail, loadUsersSuccess } from '../actions';
+import { UserModel } from '../../models/user.model';
+
+export interface UsersState {
+  users: UserModel[],
+  loaded: boolean,
+  loading: boolean,
+  error: any
+}
+
+export const usersInitialState: UsersState = {
+  users: [],
+  loaded: false,
+  loading: false,
+  error: null
+};
+
+const _usersReducer = createReducer(usersInitialState,
+  on(loadUsers, state => ({ ...state, loading: true })),
+
+  on(loadUsersSuccess, (state, { users }) => ({
+    ...state,
+    loaded: false,
+    loading: false,
+    users: [...users]
+  })),
+
+  on(loadUsersFail, (state, { payload }) => ({
+    ...state,
+    loaded: false,
+    loading: false,
+    error: payload
+  }))
+);
+
+export function usersReducer(state, action) {
+  return _usersReducer(state, action);
+}
